107019c059258e91aead6dfa55ff1107b27d48bd
arkadim <arkadimush@gmail.com>
2019-06-23 14:30:21 +0300

    first algo code

---
+++ Algo/TextRectsExtraction/rectsExtraction.py
@@ -0,0 +1,42 @@
+#import PyPDF2
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle
+
+receiptPath = 'D:\\X-tras\\ReceiptsInc\\receiptsImages\\imageTech.png'
+receiptOrigin = cv2.imread(receiptPath, cv2.IMREAD_GRAYSCALE)  # image receipt load
+
+xflt = np.array([[1, -1]])
+yflt = np.array([[1], [-1]])
+receipt = receiptOrigin.astype('int16')
+dx = cv2.filter2D(receipt, -1, xflt)
+dy = cv2.filter2D(receipt, -1, yflt)
+
+_, thDx = cv2.threshold(abs(dx), 30, 1, cv2.THRESH_BINARY)   # future idea: adaptive threshold can be considered
+_, thDy = cv2.threshold(abs(dy), 30, 1, cv2.THRESH_BINARY)   # future idea: define threshold with histogram
+thDx = np.array(thDx)
+thDy = np.array(thDy)
+textMap = np.array(thDx * thDy)
+
+gauss1D = cv2.getGaussianKernel(15, 5)      # gaussian filter generation
+gauss1D = np.reshape(gauss1D, (1, 15))      # transpose
+gaussFlt = np.array(gauss1D.repeat(3, 0))   # replicate 3 times to get 3x15 matrix
+
+textMap = np.array(cv2.filter2D(textMap, ddepth=cv2.CV_32F, kernel=gaussFlt))
+
+_, binTextMap = cv2.threshold(textMap, 0.1, 1, cv2.THRESH_BINARY)
+binTextMap = np.array(binTextMap)
+components = cv2.connectedComponentsWithStats(binTextMap.astype('byte'))
+
+rects = np.array(components[2])
+rects = rects[1:]  # remove the background rectangle, all image rect ,located at the first row
+rects = rects[rects[:, 3] > 4]
+scores = rects[:, 4] / (rects[:, 2] * rects[:, 3])  #
+
+plt.imshow(receiptOrigin, cmap='gray')
+ax = plt.gca()
+for rect in rects:
+    ax.add_patch(Rectangle((rect[0], rect[1]-1), rect[2], rect[3]+1, linewidth=1, edgecolor='r', facecolor='none'))
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
